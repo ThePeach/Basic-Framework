9aeb3c609cc73cdf07e5373f0911a2aa363f4ace
Matteo 'Peach' Pescarin <peach@smartart.it>
2011-05-01 23:27:23 +0100

    added error message handling on UI, utilities with validator

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@
 var T = T || {};
 
 /* NOTE: probably to be kept in a separate file */
+/**
+ * User Interface handling stuff
+ */
 T.gui = (function () {
     // box constructor
     function Box(node) {
 //        console.log('box constructor');
         this.domNode = node;
         this.mask = $('#screen');
-        this.setPosition();
+        this.errorMsg = '';
+//        this.setPosition();
     }
     // box prototype
     Box.prototype = (function () {
@@ -37,6 +41,19 @@ T.gui = (function () {
                 mask.fadeOut(animSpeed);
             });
         }
+        /**
+         * displays the error message
+         */
+        function displayError(errorMsg) {
+            if (errorMsg !== '' || errorMsg !== undefined) {
+                this.errorMsg = errorMsg;
+            } else if (errorMsg === false) {
+                this.errorMsg = '';
+            } else {
+                return;
+            }
+            this.domNode.children('.error').html() = this.errorMsg;                
+        }
         // private functions
         /**
          * Recalculate the position of the box and applies the style to it
@@ -52,6 +69,7 @@ T.gui = (function () {
         return {
             show: show,
             hide: hide,
+            displayError: displayError,
             setPosition: setPosition
         };
     }());
@@ -61,9 +79,19 @@ T.gui = (function () {
         'Box': Box
     };
 }());
-    
 
-// The user
+/**
+ * Utilities
+ */
+T.utils = (function () {
+    function Validator() {
+        
+    }
+});
+
+/**
+ * User
+ */
 T.user = (function () {
     // private variable definition
     var loginBox = {},
@@ -108,141 +136,3 @@ T.user = (function () {
         'init': init
     };
 }());
-
-T.popupBox = (function() {
-    var errorMessage = null,
-        top = 0,
-        left = 0;
-    var show = function(box) {
-    }
-    var close = function(box) {
-        $('#'+box+'box').hide(0, function() {
-            $('#screen').hide();
-        });
-    }
-    return {
-        'show': 'show',
-        'close': 'close'
-    }
-}());
-
-/**
- * contains the current action: login or register
- */
-T.currentAction = null;
-/**
- * contains the default mainbar html
- */
-T.mainbar = null;
-/**
- * this function will popup the login/register box
- */
-T.showBox = function(box) {
-    var boxID = '#'+box+'box';
-    T.currentAction = box;
-    // we position the box properly in the middle of the screen
-    var left = ( $('#screen').width() / 2 ) - 150;
-    var top  = ( $('#screen').height() / 2 ) - ( $(boxID).height() / 2 )
-    $(boxID).attr('style','top: '+top+'px; left: '+left+'px;');
-    // then we show everything
-    $('#screen').show(100, function() {
-        $(boxID).show();
-    });
-    $(boxID).submit(function() {
-        var allFine = true;
-        var data = 'req='+box;
-        // validate fields!
-        if ($(boxID+' input[name="pwd"]').val() === '') {
-            $(boxID+' input[name="pwd"]').attr('style','border: 1px solid red;');
-            // we would like to write some nice error messages in the error box...
-            allFine = false;
-        } else {
-            $(boxID+' input[name="pwd"]').attr('style','border: 0px;');
-        }
-        if ($(boxID+' input[name="login"]').val() === '') {
-            $(boxID+' input[name="login"]').attr('style','border: 1px solid red;');
-            allFine = false;
-        } else {
-            $(boxID+' input[name="login"]').attr('style','border: 0px;');
-        }
-        // if we are registering ensure a couple more things are ok before submitting
-        if (T.currentAction === 'register') {
-            if ($(boxID+' input[name="cc"]').val() === ''
-                || $(boxID+' input[name="cc"]').val().length != 16) {
-                $(boxID+' input[name="cc"]').attr('style','border: 1px solid red;');
-                allFine = false;
-            } else {
-                $(boxID+' input[name="cc"]').attr('style','border: 0px;');
-            }
-            if ($(boxID+' input[name="name"]').val() === '') {
-                $(boxID+' input[name="name"]').attr('style','border: 1px solid red;');
-                allFine = false;
-            } else {
-                $(boxID+' input[name="name"]').attr('style','border: 0px;');
-            }
-            if ($(boxID+' input[name="pwd2"]').val() === '') {
-                $(boxID+' input[name="pwd2"]').attr('style','border: 1px solid red;');
-                allFine = false;
-            } else {
-                $(boxID+' input[name="pwd2"]').attr('style','border: 0px;');
-            }
-            // passwords mismatch
-            if ($(boxID+' input[name="pwd"]').val() !== $(boxID+' input[name="pwd2"]').val()) {
-                $(boxID+' input[name="pwd2"]').attr('style','border: 1px solid red;');
-                allFine = false;
-            }
-        }
-        // hash the pwd before sending it!!!
-        var hashPwd = $.sha1($(boxID+' input[name="pwd"]').val());
-        // now compose the post fields to be sent
-        data = data+'&login='+$(boxID+' input[name="login"]').val();
-        data = data+'&pwd='+hashPwd;
-        if (T.currentAction === 'register') {
-            data = data+'&user='+$(boxID+' input[name="name"]').val();
-            data = data+'&cc='+$(boxID+' input[name="cc"]').val();
-        }
-        // if everything was fine send the data, otherwise display something
-        if (!allFine) {
-            $(boxID+' .error').html('Please correct the errors')
-        } else {
-            $(boxID+' .error').html('');
-            // saving what can be saved
-            T.user.email = $(boxID+' input[name="login"]').val();
-            if (T.currentAction === 'register') {
-                T.user.name = $(boxID+' input[name="name"]').val();
-                T.user.cc = $(boxID+' input[name="cc"]').val();
-            }
-            $.post('index.php', data, function(success) {
-                // verify the returned json contains no errors otherwise act properly
-                if (success.error !== null) {
-                    $('#'+T.currentAction+'box .error').html(success.error);
-                } else {
-                    // saving user and cc
-                    if (T.currentAction === 'login') {
-                        T.user.name = success.result.name;
-                        T.user.cc = success.result.ccnumber;
-                    }
-                    // replace the mainbar with something more meaningful
-                    T.welcomeUser();
-                    // remove the popup box
-                    T.closeBox(T.currentAction);
-                }
-            }, 'json');
-        }
-        return false;
-    });
-};
-/**
- * This function will add a greeting to the user and substitute the
- * content of the mainbar
- */
-T.welcomeUser = function() {
-    // save content of mainbar
-    if (null === T.mainbar) {
-        T.mainbar = $('#mainbar').html();
-    }
-    $('#mainbar li:first').html('Welcome '+T.user.name);
-    $('#mainbar li a').html('Logout');
-    $('#mainbar li a').attr('title','logout');
-    $('#mainbar li a').attr('onClick','javascript:T.logout(); return false;');
-}
