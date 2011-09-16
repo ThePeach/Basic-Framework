076e1d4734f38ea4a1479a5a5fa6282f3bca68c2
Matteo 'Peach' Pescarin <peach@smartart.it>
2011-09-16 08:04:01 +0100

    updated sql test file

--- test.sql
+++ test.sql
@@ -1,13 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.8.1
+-- version 3.4.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 03, 2011 at 12:22 AM
--- Server version: 5.1.51
+-- Generation Time: Sep 15, 2011 at 11:09 PM
+-- Server version: 5.1.56
 -- PHP Version: 5.2.17-pl0-gentoo
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `test`
@@ -23,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(255) NOT NULL,
   `name` text NOT NULL,
   `password` text NOT NULL,
-  `ccnumber` bigint(16) unsigned NOT NULL,
+  `ccnumber` bigint(16) unsigned DEFAULT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -32,4 +39,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`email`, `name`, `password`, `ccnumber`) VALUES
-('test@test.com', 'Baz', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 234500012340000);
+('test@test.com', 'Baz', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
